60ea3e4be4a77e26e30b365eca4683b2f2ccf178
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-09-07 12:02:07 +0530

    4 body kinematic movement on time

---
+++ scripts/Car4.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+#if Input.is_action_pressed("ui_right"):
+#	motion.x = 100
+#elif Input.is_action_just_released("ui_right"):
+#	motion.x = 0
+#if Input.is_action_pressed("ui_left"):
+#	motion.x = -100
+#elif Input.is_action_just_released("ui_left"):
+#	motion.x = 0
+#if Input.is_action_pressed("ui_up"):
+#	motion.y = -100
+#elif Input.is_action_just_released("ui_up"):
+#	motion.y = 0	
+#if Input.is_action_pressed("ui_down"):
+#	motion.y = 100
+#elif Input.is_action_just_released("ui_down"):
+#	motion.y = 0
+#move_and_slide(motion)
+
+var motion = Vector2()
+var speed = 50
+
+func _process(delta):
+	motion.x = speed 
+	move_and_slide(motion)
+	pass
+	
+
