d4a7fad5b7a7d7d8d22ab3fbfa9c48b429d68500
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-09-10 18:16:39 +0530

    animation added

--- scripts/Play.gd
+++ scripts/Play.gd
@@ -1,16 +1,10 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_Start_pressed():
+	Transition.fade_to("res://Main.tscn")
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass
+	
