03b479e70a14bf12a1607b3239d694cb07352ff6
KILI <kili@localhost.localdomain>
2020-09-13 14:20:27 +0530

    Pause script updated

--- Pause.gd
+++ Pause.gd
@@ -1,1 +1,14 @@
+extends Control
 
+func _ready():
+	print("paused")
+	get_tree().paused = true
+
+func _process(delta):
+	get_input()
+	pass
+	
+func get_input():
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = false
+		queue_free()
